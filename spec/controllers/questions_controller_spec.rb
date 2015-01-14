fb2b216bed7f75631b994c44eb851a7c544f223a
Stephen Clem <stevebclem@gmail.com>
2015-01-13 20:12:40 -0800

    completed release 2

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, :type => :controller do
 
+
+  describe 'QuestionsController' do
+    context '#index' do
+      it 'returns a status of 200 - O.K.' do
+        get :index
+        expect(response.status).to eq(200)
+      end
+
+      it 'displays all the questions' do
+        get :index
+        expect(assigns(:questions)).to eq Question.all
+      end
+    end
+
+    context '#create' do
+
+      it 'creates a new instance of Question' do
+        get :new
+        expect()
+      end
+    end
+
+    # context '#show'
+
+
+  end
+
 end
+
